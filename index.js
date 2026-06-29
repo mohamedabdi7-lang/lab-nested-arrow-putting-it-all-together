fe9c6ed78e438e0d4cd811b8aa47153df31f1f1b
mohamedabdi7-lang <mohamedsiraj2025k@gmail.com>
2026-06-29 20:43:30 +0300

    Adding to git

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  const login = (passwordAttempt) => {
+    attemptCount++;
 
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+
+    return `Login attempt ${attemptCount}: Login failed`;
+  };
+
+  return login;
+}
+
+     
